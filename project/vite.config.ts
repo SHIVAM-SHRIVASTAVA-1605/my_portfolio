c423f5d305c09ade4e92c41e73e8a434f460ca06
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-04-28 10:52:36 +0530

    first commit

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/your-repo-name/',  // <-- add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
